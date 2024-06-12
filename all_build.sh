d00d69a304ceca432993fe5c39cd867d0be406ea
ab25cq <ab25cq@icloud.com>
2024-06-12 08:07:54 +0000

    mf

--- all_build.sh
+++ all_build.sh
@@ -1,1 +1,1 @@
-(echo vin && cd vin && bash fast_build.sh) && (echo zed && cd zed && bash fast_build.sh) && (echo shsh && cd shsh && bash fast_build.sh) && (echo mf && cd mf && neo-c header && neo-c compile)
+(echo vin && cd vin && bash fast_build.sh) && (echo zed && cd zed && bash fast_build.sh) && (echo shsh && cd shsh && bash fast_build.sh) && (echo mf && cd mf && neo-c header && neo-c compile && sudo neo-c install)
